0802e85684c6712b923fd9bfe923454a9ee54e81
Lypzis <victor.piccoli@pm.me>
2024-07-18 21:09:22 -0300

    Updates

---
+++ precache-manifest.c3b9ea87c6dfdcd41ca4e1d6d8972081.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5ed8feae4860f91e338c3886d30e954",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d52b32c3f5afc74b091",
+    "url": "/static/css/2.1a0a2c47.chunk.css"
+  },
+  {
+    "revision": "775eb84bd545d0586254",
+    "url": "/static/css/main.631b607b.chunk.css"
+  },
+  {
+    "revision": "2d52b32c3f5afc74b091",
+    "url": "/static/js/2.506d218a.chunk.js"
+  },
+  {
+    "revision": "26c9ffce05ad8158631a89baca797013",
+    "url": "/static/js/2.506d218a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "775eb84bd545d0586254",
+    "url": "/static/js/main.f41ce48a.chunk.js"
+  },
+  {
+    "revision": "a0a694e2ccb77b55cf34",
+    "url": "/static/js/runtime-main.5f5f25a0.js"
+  },
+  {
+    "revision": "5f013c6e30d1eea620be4f3b23e952d6",
+    "url": "/static/media/Jaldi-Bold.5f013c6e.ttf"
+  },
+  {
+    "revision": "f0884788a6720ae0732c422d174c843c",
+    "url": "/static/media/Jaldi-Regular.f0884788.ttf"
+  },
+  {
+    "revision": "948114af9fe60735a6de25fa281c190c",
+    "url": "/static/media/KOMIKAX_.948114af.ttf"
+  },
+  {
+    "revision": "fd5134d8c099d6cff00804961f38c4a5",
+    "url": "/static/media/Rotating_Product_Banner_Finals_1.fd5134d8.png"
+  },
+  {
+    "revision": "27b28cc75442d66ae5faf180d0632df4",
+    "url": "/static/media/Rotating_Product_Banner_Finals_2.27b28cc7.jpg"
+  },
+  {
+    "revision": "96d424c77ccebd52b694a125d4e281e6",
+    "url": "/static/media/Rotating_Product_Banner_Finals_3.96d424c7.jpg"
+  },
+  {
+    "revision": "b6831e09521e347aa3c43c9aa43c0506",
+    "url": "/static/media/Rotating_Product_Banner_Finals_4.b6831e09.jpg"
+  },
+  {
+    "revision": "4e3eeffa02264ccfd8bf4624c524f093",
+    "url": "/static/media/body.4e3eeffa.png"
+  },
+  {
+    "revision": "bc5d9842182707211de70551ad3edd0a",
+    "url": "/static/media/footer.bc5d9842.png"
+  },
+  {
+    "revision": "7cd30f6ee5cd960eee4497fcf0c2dd46",
+    "url": "/static/media/header.7cd30f6e.png"
+  },
+  {
+    "revision": "a170f855e83d4c861003642c924f12ef",
+    "url": "/static/media/logo.a170f855.png"
+  },
+  {
+    "revision": "afcd1ab0bd28776f36d35a79f7d66075",
+    "url": "/static/media/sprite.afcd1ab0.svg"
+  }
+]);
